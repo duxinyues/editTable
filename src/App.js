9d5e8555871f8c5a392a5e889a3304b348382762
duxinyues <yongyuan253015@gmail.com>
2021-06-30 00:58:20 +0800

    编辑antd表格

--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+ * @FileName: 
+ * @Author: 1638877065@qq.com
+ * @Date: 2021-06-29 23:50:37
+ * @LastEditors: 1638877065@qq.com
+ * @LastEditTime: 2021-06-30 00:56:57
+ * @FilePath: \edittable\src\App.js
+ * @Description: b编辑表格
+ */
+import React, { useState,useEffect } from 'react';
+import { Table, Input, InputNumber, Form } from 'antd';
+const originData = [];
+document.title = "编辑表格"
+for (let i = 0; i < 5; i++) {
+  originData.push({
+    key: i.toString(),
+    name: `Edrward ${i}`,
+    age: 32,
+    address: `London Park no. ${i}`,
+  });
 }
 
-export default App;
+const EditableCell = ({
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+// console.log("dataIndex:",dataIndex)
+// console.log("title:",title)
+// console.log("inputType:",inputType)
+// console.log("index:",index)
+  return (
+    <td {...restProps}>
+      {editing ? (
+        <Form.Item
+          name={dataIndex+index}
+          style={{
+            margin: 0,
+          }}
+          rules={[
+            {
+              required: true,
+              message: `Please Input ${title}!`,
+            },
+          ]}
+        >
+         <Input />
+        </Form.Item>
+      ) : (
+        children
+      )}
+    </td>
+  );
+};
+
+const EditableTable = () => {
+  const [form] = Form.useForm();
+  const [data, setData] = useState(originData);
+  const [editingKey, setEditingKey] = useState([0,1,2,3]);
+  useEffect(()=>{
+    data.map((item)=>{})
+  },[])
+
+  const edit = (record) => {
+    console.log(record)
+    console.log(data)
+    form.setFieldsValue({
+      name: '12',
+      age: '2',
+      address: '43',
+      ...record,
+    });
+  };
+
+ 
+
+  const save = async (key) => {
+    try {
+      const row = await form.validateFields();
+    } catch (errInfo) {
+      console.log('Validate Failed:', errInfo);
+    }
+  };
+
+  const columns = [
+    {
+      title: 'name',
+      dataIndex: 'name',
+      width: '25%',
+      editable: true,
+    },
+    {
+      title: 'age',
+      dataIndex: 'age',
+      width: '15%',
+      editable: true,
+    },
+    {
+      title: 'address',
+      dataIndex: 'address',
+      width: '40%',
+      editable: true,
+    },
+  ];
+  const mergedColumns = columns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+    console.log("col==",col)
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        inputType: col.dataIndex === 'age' ? 'number' : 'text',
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: true,
+      }),
+    };
+  });
+  return (<>
+    <Form form={form} component={false}>
+      <Table
+        components={{
+          body: {
+            cell: EditableCell,
+          },
+        }}
+        dataSource={data}
+        columns={mergedColumns}
+        rowClassName="editable-row"
+      />
+    </Form>
+      <button onClick = {save}>保存</button>
+    </>
+  );
+};
+  export  default EditableTable
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+/*
+ * @FileName: 
+ * @Author: 1638877065@qq.com
+ * @Date: 2021-06-29 23:50:37
+ * @LastEditors: 1638877065@qq.com
+ * @LastEditTime: 2021-06-30 00:10:08
+ * @FilePath: \edittable\src\index.js
+ * @Description: 
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import EditableTable from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <EditableTable />
   </React.StrictMode>,
   document.getElementById('root')
 );
