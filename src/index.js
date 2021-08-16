c1ad7551c0dbe705f304080121722feff97e0e50
duxinyues <yongyuan253015@gmail.com>
2021-08-16 22:50:36 +0800

    新增边个编辑

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@
  * @FileName: 
  * @Author: 1638877065@qq.com
  * @Date: 2021-06-29 23:50:37
- * @LastEditors: 1638877065@qq.com
- * @LastEditTime: 2021-06-30 00:10:08
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-08-16 22:45:21
  * @FilePath: \edittable\src\index.js
  * @Description: 
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import EditableTable from './App';
+import EditableTable from './edit-table';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
