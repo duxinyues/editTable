ea042a746138c74f59467c60983a918928376a3a
读心悦 <yongyuan253015@gmail.com>
2022-01-17 21:37:30 +0800

    分类

---
+++ editTable/src/index.js
@@ -0,0 +1,26 @@
+/*
+ * @FileName: 
+ * @Author: 1638877065@qq.com
+ * @Date: 2021-06-29 23:50:37
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-09-08 22:24:10
+ * @FilePath: \edittable\src\index.js
+ * @Description: 
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import EditableTable from './edit-table';
+// import AutoTable  from './autoTable';
+import reportWebVitals from './reportWebVitals';
+ReactDOM.render(
+  <React.StrictMode>
+    <EditableTable />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
